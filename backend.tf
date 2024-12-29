08397ab42d16896a685fe3a6a25e578e67faf46a
Raja menikonda <90019456+vardhanr49@users.noreply.github.com>
2024-12-28 21:55:43 -0600

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# backend.tf
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket"
-    key            = "pharma-infra/terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
